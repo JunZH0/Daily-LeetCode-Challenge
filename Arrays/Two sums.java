9cdec3e8968e1745cd8b8abe5ddb816e10708cc2
Jun Zhou <54965484+JunZH0@users.noreply.github.com>
2024-03-13 15:42:06 +0100

    Update Two sums.java

--- Arrays/Two sums.java
+++ Arrays/Two sums.java
@@ -1,4 +1,6 @@
 /*
+Difficulty: Easy
+
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
@@ -43,3 +45,16 @@ class Solution {
         return new int[0];
     }
 }
+
+/* Time and Space complexity
+TC: 
+
+
+SC:
+
+
+Aproach
+
+
+
+*/
