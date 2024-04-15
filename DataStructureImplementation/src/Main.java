cbda854a86ea0cb5f0cf9822d98ccb4e4fe82e1b
Jun Zhou <zhoujun97099@gmail.com>
2024-04-15 22:43:36 +0200

    Added DataStructureImplementation folder, and LinkedList implementation

---
+++ DataStructureImplementation/src/Main.java
@@ -0,0 +1,12 @@
+import LinkedList.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.removeLast();
+        System.out.println(list.contains(10));
+    }
+}
