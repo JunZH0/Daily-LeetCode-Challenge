f4e9e2780c49bfd2e9adadea3075e27a2983bd0f
Jun Zhou <zhoujun97099@gmail.com>
2024-05-10 23:52:17 +0200

    Added PriorityQueue

--- DataStructureImplementation/src/Main.java
+++ DataStructureImplementation/src/Main.java
@@ -1,19 +1,18 @@
 import LinkedList.LinkedList;
+import Queue.PriorityQueue;
 import Queue.QueueWithTwoStack;
 import Stack.Stack;
 import Stack.StringReverser;
 
 public class Main {
     public static void main(String[] args) {
-        QueueWithTwoStack queue = new QueueWithTwoStack();
-        queue.enqueue(10);
-        queue.enqueue(20);
-        queue.enqueue(30);
-        queue.dequeue();
-        queue.dequeue();
-        queue.dequeue();
-        var first = queue.dequeue();
+        PriorityQueue queue = new PriorityQueue();
+        queue.add(5);
+        queue.add(3);
+        queue.add(6);
+        queue.add(4);
+        queue.add(1);
 
-        System.out.println(first);
+        System.out.println(queue);
     }
 }
